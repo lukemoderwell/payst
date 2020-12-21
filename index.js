f7da1b4cfc5c57c4647c7f0587496da11820d912
Luke Moderwell <luke.moderwell@gmail.com>
2020-12-21 16:06:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,14 +23,21 @@ function pasteLink(e) {
       navigator.permissions.query({ name: "clipboard-read" }).then(result => {
         // If permission to read the clipboard is granted or if the user will
         // be prompted to allow it, we proceed.
-
         if (result.state == "granted" || result.state == "prompt") {
           navigator.clipboard.readText().then(data => {
-            // check that data is a url
+            // check that data is either an image or url
+            var imageRe = /\.(gif|jpe?g|tiff?|png|webp|bmp)$/i;
             var urlRe = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-            if (urlRe.test(data)) {
-              var link = '[' + selectedText +'](' + data + ')';
-              var textarea = window.getSelection().focusNode.parentElement.getElementsByTagName('textarea')[0];
+            var link;
+            var textarea = window.getSelection().focusNode.parentElement.getElementsByTagName('textarea')[0];
+            if (imageRe.test(data)) {
+              link = '![' + selectedText +'](' + data + ')';
+              // select entire range & set link
+              textarea.setSelectionRange(0,-1);
+              textarea.setRangeText(link);      
+              textarea.blur();
+            } else if (urlRe.test(data)) {
+              link = '[' + selectedText +'](' + data + ')';
               // select entire range & set link
               textarea.setSelectionRange(0,-1);
               textarea.setRangeText(link);      
